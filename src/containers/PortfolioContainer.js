1ad95cff6ed450ddb7247c7c7473345b718a52e3
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-29 17:57:18 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,16 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderStocks = () => {
+    return this.props.stocks.map(stock => <Stock portfolio={this.props.portfolio} key={stock.id} stock={stock} />)
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.renderStocks()
           }
       </div>
     );
