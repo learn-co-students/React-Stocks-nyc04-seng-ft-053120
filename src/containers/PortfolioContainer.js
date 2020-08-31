e7b14c54b09fde2a236c6a8e9c061e0551e4bce2
Jackson Chen <jacksonchen@Jacksons-Air.home>
2020-08-31 16:49:54 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,13 +3,21 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderStocks = () => {
+    return this.props.stocks.map(stockObj => (
+    <Stock 
+    key={stockObj.id}
+    stock={stockObj}
+    handleClick={this.props.handleClick}
+    />
+    ))
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {this.renderStocks()}
       </div>
     );
   }
