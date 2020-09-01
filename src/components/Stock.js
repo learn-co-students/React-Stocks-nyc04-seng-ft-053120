c1c44ac31bdd6b811d570711b320b1bbd3ea0b38
21shield <21netaly@gmail.com>
2020-09-01 16:47:25 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,33 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => {
+  const {name, price, ticker, id} = props.stock
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+  let handleClick = (e)=>{
+    // pass id to the passed down function?
+       console.log(props)
+       props.state ? props.toggleSell(props.stock.name) : props.toggleBought(props.stock.name)
+  //  return props.toggleBought(props.stock.name)
+  }
+
+  return(
+    <div>
+      {/*  */}
+      <div className="card" onClick={handleClick} >
+        <div className="card-body" >
+          <h5 className="card-title">{
+              name
+            }</h5>
+          <p className="card-text">{
+            ` ${ticker}: ${price}`
+            }</p>
+        </div>
       </div>
+
+
     </div>
 
-
-  </div>
-);
+  )
+}
 
 export default Stock
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,71 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state = {
+    stocks:[],
+    portfoly: [{name: "fran"}],
+    filterType: "Tech"
+  }
+
+  stockData = () => {
+    fetch("http://localhost:3000/stocks")
+    .then(resp => resp.json())
+    .then(info =>{
+      this.handleStockInfo(info)
+    })
+  }
+
+  handleStockInfo = (arr) => {
+    this.setState({stocks: [...arr]})
+  }
+  
+  componentDidMount(){
+    this.stockData()
+  }
+ 
+  addStock = (name) => {
+    
+    // add the obj wiht the matching name to the states portfoly array
+   let portfolyStock = this.state.stocks.find((stock) => stock.name === name)
+    this.setState((prev) => ({
+      portfoly: [...prev.portfoly, portfolyStock]
+    })
+    )
+  }
+  
+  sellStock = (name) => {
+    console.log('WE SELL NOW',name)
+    const updatedArr = this.state.portfoly.filter(stock => stock.name !== name)
+    this.setState({
+      portfoly: updatedArr
+    })
+  }
+  
+  handleFilter = (type) => {
+    this.setState({
+      filterType: type
+    })
+  }
+  
+  
 
   render() {
+    let stockArr = this.state.stocks
+    console.log("object", this.state)
+    let filter = this.state.stocks.filter((stock) => stock.type === this.state.filterType )
     return (
       <div>
-        <SearchBar/>
+        <SearchBar filter={this.handleFilter}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={filter} toggleBought={this.addStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolyArray={this.state.portfoly} toggleSell={this.sellStock}/>
 
             </div>
           </div>
